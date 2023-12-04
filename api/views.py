06fc8bfc02badac878fd120bbec0f69a60d1e052
feraranas <46a12sFAASfaas>
2023-12-03 23:24:10 -0600

    dark-light mode

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import JsonResponse
 from datetime import timedelta
+from api.serializers import UserSerializer
 from rental.models import Game, Plays, Student, Log, Sanction
 from django.contrib.auth.models import User, Group
 from django.views.decorators.http import require_http_methods
@@ -486,3 +487,23 @@ def user(request):
             return JsonResponse({'status': 'error', 'message': 'User not found'})
         except Exception as e:
             return JsonResponse({'status': 'error', 'message': str(e)})
+        
+
+def userSettings(request):
+	user, created = User.objects.get_or_create(id=1)
+	setting = user.setting
+
+	seralizer = UserSerializer(setting, many=False)
+
+	return JsonResponse(seralizer.data, safe=False)
+
+
+def updateTheme(request):
+	data = json.loads(request.body)
+	theme = data['theme']
+	
+	user, created = User.objects.get_or_create(id=1)
+	user.setting.value = theme
+	user.setting.save()
+	print('Request:', theme)
+	return JsonResponse('Updated..', safe=False)
--- static/js/dark_light_mode.js
+++ static/js/dark_light_mode.js
@@ -1,101 +1,63 @@
-// function getCookie(name) {
-//     var cookieValue = null;
-//     if (document.cookie && document.cookie !== '') {
-//         var cookies = document.cookie.split(';');
-//         for (var i = 0; i < cookies.length; i++) {
-//             var cookie = cookies[i].trim();
-//             // Does this cookie string begin with the name we want?
-//             if (cookie.substring(0, name.length + 1) === (name + '=')) {
-//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//                 break;
-//             }
-//         }
-//     }
-//     return cookieValue;
-// }
-// var csrftoken = getCookie('csrftoken');
-
-// var cssFile = "{% static 'css' %}"
-
-// function swapStyles(){
-     // if (document.body.classList.contains('dark-mode')) {
-     //      document.body.classList.remove('dark-mode');
-     //      document.body.classList.add('light-mode');
-     // } else if (document.body.classList.contains('light-mode')) {
-     //      document.body.classList.remove('light-mode');
-     //      document.body.classList.add('dark-mode');
-     
-     // }
-     // document.body.classList.toggle('dark-mode');
-     // document.body.classList.toggle('light-mode');
-
-     // Save the current mode preference in localStorage
-     // const currentMode = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
-     // localStorage.setItem('theme', currentMode);
-
-     // document.getElementById('mystylesheet').href = cssFile + '/' + sheet
-     // localStorage.setItem('theme', sheet)
-
-     // updateTheme(sheet)
-// }
-
-function swapStyles() {
-     document.body.classList.toggle('dark-light-mode');
-     const currentMode = document.body.classList.contains('dark-light-mode') ? 'light' : 'dark';
-     localStorage.setItem('theme', currentMode);
- }
- 
-
-function loadInitialTheme() {
-
-     const savedTheme = localStorage.getItem('theme');
-     
-     if (savedTheme === 'light') {
-         document.body.classList.add('dark-light-mode');
+function getCookie(name) {
+     var cookieValue = null;
+     if (document.cookie && document.cookie !== '') {
+         var cookies = document.cookie.split(';');
+         for (var i = 0; i < cookies.length; i++) {
+             var cookie = cookies[i].trim();
+             // Does this cookie string begin with the name we want?
+             if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                 break;
+             }
+         }
      }
-     
+     return cookieValue;
+ }
+ var csrftoken = getCookie('csrftoken');
+
+ var sassFile = "{% static 'css' %}"
+
+function swapStyles(sheet) {
+     document.getElementById('mode-stylesheet').href = sassFile + '/' + sheet
+     localStorage.setItem('theme', sheet);
+     updateTheme(sheet);
+} 
+
+function loadSettings(){
+     var url = "{% url 'user_settings' %}"
+     fetch(url, {
+          method:'GET',
+          headers:{
+               'Content-type':'application/json'
+          }
+     })
+     .then((response) => response.json())
+     .then(function(data){
+
+          console.log('Data:', data)
+
+          var theme = data.value;
+          
+          if (theme == 'main-light.min.css' || null){
+               swapStyles('main-light.min.css')
+          }else if(theme == 'main.min.css'){
+               swapStyles('main.min.css')
+          }
+     })
+
 }
- 
- loadInitialTheme();
 
-// function loadSettings(){
-//      //Call data and set local storage
+loadSettings()
 
-//      var url = "{% url 'user_settings' %}"
-//      fetch(url, {
-//           method:'GET',
-//           headers:{
-//                'Content-type':'application/json'
-//           }
-//      })
-//      .then((response) => response.json())
-//      .then(function(data){
 
-//           console.log('Data:', data)
-
-//           var theme = data.value;
-          
-//           if (theme == 'light.css' || null){
-//                swapStyles('light.css')
-//           }else if(theme == 'dark.css'){
-//                swapStyles('dark.css')
-//           }
-          
-
-//      })
-
-// }
-
-// loadSettings()
-
-// function updateTheme(theme){
-//      var url = "{% url 'update_theme' %}"
-//      fetch(url, {
-//           method:'POST',
-//           headers:{
-//                'Content-type':'application/json',
-//                'X-CSRFToken':csrftoken,
-//           },
-//           body:JSON.stringify({'theme':theme})
-//      })
-// }
+function updateTheme(theme){
+	var url = "{% url 'update_theme' %}"
+	fetch(url, {
+		method:'POST',
+		headers:{
+			'Content-type':'application/json',
+			'X-CSRFToken':csrftoken,
+		},
+		body:JSON.stringify({'theme':theme})
+	})
+}
