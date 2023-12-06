ca4f4fd4cd13f70c441305f2aef2c4f267e5dbfc
feraranas <46a12sFAASfaas>
2023-12-06 14:55:09 -0600

    merge

--- api/views.py
+++ api/views.py
@@ -182,6 +182,23 @@ def add_student_to_game(request):
             if created:
                 student.save()
             
+            if Plays.objects.filter(student__id=student_id, ended=False).exists():
+                # Get the latest play for the student that is not ended, meaning the student is currently playing
+                play = Plays.objects.filter(student__id=student_id, ended=False).latest('time')
+
+                # Save the corresponding game start_time before changing the game of the play
+                old_game_time = play.game.start_time
+
+                # Change the game of the play to the new game_id and save it
+                play.game = game
+                play.save()
+                log = Log.objects.create(actionPerformed=f' Cambia juego de: {student_id}', user=request.user)
+                log.save()
+                game.start_time = old_game_time
+                game.save()
+
+                return JsonResponse({'status': 'success', 'message': 'Alumno cambiado de juego'})
+
             if condiciones_para_juego(student_id):
                 play = Plays.objects.create(game=game, student=student)
                 play.save()
