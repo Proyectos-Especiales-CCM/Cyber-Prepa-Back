f7c6869e7912b484e0e9c31dac6969874cf8ade2
Diego Jacobo Martínez <diegojmr5@gmail.com>
2024-09-19 14:20:57 -0600

    Fix failing tests
      Tests were failing because the postgresql database wasn't available on the
      testing environment. So we added a new env var to allow selection of database

--- main/settings.py
+++ main/settings.py
@@ -91,21 +91,24 @@ WSGI_APPLICATION = "main.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get(
-            "DB_HOST", "localhost"
-        ),  # Defaults to localhost if not set
-        "PORT": os.environ.get("DB_PORT", "5432"),  # Defaults to 5432 if not set
-    },
-    "OPTIONS": {
-        "sslmode": "disable",
-    },
-}
+if os.environ.get('TESTING') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
